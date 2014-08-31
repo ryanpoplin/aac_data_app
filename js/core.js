261aabf62da4d00820afb30d8394f955c85c8459
Byrdann Fox <mobileupstate@gmail.com>
2014-08-30 22:56:19 -0400

    Update...

--- js/core.js
+++ js/core.js
@@ -1,20 +1,67 @@
 'use strict';
 (function($) {
+  var logicGate = true;
   Parse.initialize("QVQ9y2NgrPvf4AnkKCdtGgiqrrFErrRJNd9BdTi6", "itZhGux4XOLo5jkLBJVeNKpkJBRH2XSgLLdhAKmE");  
+  var LogInView;
+  LogInView = Backbone.View.extend({
+    tagName: 'div',
+    el: '#default-template-section',
+    template: _.template($('#log-in-template').html()),
+    events: {
+      'click .new-account': 'signUpRender'
+    },
+    render: function() {
+      logicGate = true;
+      $(this.el).html(this.template({}));
+    },
+    signUpRender: function() {
+      logicGate = false;
+      var signUpView = new SignUpView({});  
+      signUpView.render();
+      $(defaultView.el).append(logInView.$el);
+    }
+  });
+  var SignUpView;
+  SignUpView = Backbone.View.extend({
+    tagName: 'div',
+    el: '#default-template-section',
+    template: _.template($('#sign-up-template').html()),
+    render: function() {
+      logicGate = false;
+      $(this.el).html(this.template({}));
+    },
+    loginInRender: function() {
+      logicGate = true;
+      var logInView = new logInView({});  
+      logInView.render();
+      $(defaultView.el).append(signUpView.$el);
+    }
+  });
   var DefaultView;
   DefaultView = Backbone.View.extend({
     tagName: 'div',
     el: '#spa',
-    template: _.template($('#default-view').html()),
+    logIn: '#default-template-section',
+    signUp: '#default-template-section',
+    template: _.template($('#default-template').html()),
     render: function() {
-      $(this.el).html(this.template({}));
-    }
+      // if (logicGate) {
+        $(this.el).html(this.template({}));
+        var logInView = new LogInView({});  
+        logInView.render();
+        $(this.logIn).append(logInView.$el);
+      /*} else {
+        $(this.el).html(this.template({}));
+        var signUpView = new SignUpView({});  
+        signUpView.render();
+        $(this.signUp).append(signUpView.$el);
+      }*/
   });
   var DataView;
   DataView = Backbone.View.extend({
     tagName: 'div',
     el: '#spa',
-    template: _.template($('#data-view').html()),
+    template: _.template($('#data-template').html()),
     render: function() {
       $(this.el).html(this.template({}));
     }
